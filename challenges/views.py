4025c1fe6bf41076d685bede90705f37ede0d1b7
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-09-20 23:10:46 +0300

    c11

--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     'january': 'Walk for at least 10 minutes every day!',
@@ -23,7 +24,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound('Invalid month.')
 
     redirect_month = months[month - 1]
-    return HttpResponseRedirect('/challenges/' + redirect_month)
+    redirect_path = reverse(viewname="month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
