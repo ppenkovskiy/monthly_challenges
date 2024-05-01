14ceeac8a442a9882f6ec55072ef070059b0a1ad
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-05-01 15:42:13 +0300

    fixed_index.html

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-1
+Project "Challenges" preview:
+
+...
+
+Installing "Challenges" project on Linux:
+
+git clone git@github.com:ppenkovskiy/monthly_challenges.git
+
+cd monthly_challenges
+
+sudo apt install python3.10-venv
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+python3 manage.py migrate
+
+python3 manage.py runserver
--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-te=cy(q*nv+ku$q@7mc$(8x==*mgkc^fbpi&s_w=pbbj=-xayl'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
